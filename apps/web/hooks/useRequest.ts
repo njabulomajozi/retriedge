c239dfad4a506d532bc5152abb0650c6d42dce1d
Njabulo <services@njabulomajozi.com>
2024-04-28 22:28:57 +0200

    chore: Use base url from env

--- apps/web/hooks/useRequest.ts
+++ apps/web/hooks/useRequest.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const baseUrl = 'http://localhost:3000/api';
+const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
 
 interface IUseRequestParam {
 	path: string;
